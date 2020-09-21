cb9c7de768d628aed89129b67a824c8ccb1348f9
Subash KC <subash.kc4795@gmail.com>
2020-09-21 19:36:28 +0000

    manipulating lists

---
+++ lunch.py
@@ -0,0 +1,5 @@
+challenge_list =["awesome", "terrible", "supoib", "Python", "banana", 1, 2]
+
+
+# Python is awesome! For lunch I will have 2 bananas.\
+
---
+++ solution.py
@@ -0,0 +1,6 @@
+hero= "Fat Thor"
+villian = "Joker"
+
+print("My favorite superhero is " + hero + " and my favorite villian is " + villain + ".")
+
+print(f"My favorite superhero is {hero} and my favorite villian is {villain}. ")
