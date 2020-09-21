a056422771c2df152b4c7562036b8752733b6283
Subash KC <subash.kc4795@gmail.com>
2020-09-21 17:10:51 +0000

    My commit before lunch

---
+++ no_shebang.py
@@ -0,0 +1,8 @@
+print("ello!")
+print("Did you say, hello?")
+#print("No, I said ello, but that\'s close enough.")
+
+
+# function <-- repeatable block of code
+# method <-- function that's only callable by a certain class.
+
